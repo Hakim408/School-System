852c8c97408ccc0b3fe925f8ceb471560a7d2a8e
Hakim408 <hakimraut408@gmail.com>
2024-11-18 09:14:29 +0545

    binary search

--- admin_panel/searchFunction.php
+++ admin_panel/searchFunction.php
@@ -1,22 +1,39 @@
 <?php
 $searches = array('dashboard', 'teacher', 'student', 'subjects', "attendence", 'noticeboard', 'timetable', 'syllabus', 'notes', 'marks','bus service', 'settings');
-
 $pages = array('dashboard.php', 'teacher.php', 'student.php', 'subjects.php', "attendence.php", 'noticeboard.php', 'timetable.php', 'syllabus.php', 'notes.php', 'marks.php','buses.php', 'settings.php');
 
+// Create an associative array to map searches to pages
+$searchMap = array_combine($searches, $pages);
+
+// Sort the array alphabetically by the search keys
+ksort($searchMap);
+
+$searches = array_keys($searchMap); // Sorted keys
+$pages = array_values($searchMap); // Sorted values
+
 $response = "";
+
 if (isset($_POST['searchValue'])) {
     $searchValue = $_POST['searchValue'];
-    $i = 0;
-    foreach ($searches as $search) {
-        if ($searchValue !== '' && str_contains($search, $searchValue)) {
 
-            echo $pages[$i];
+    // Binary search implementation
+    $low = 0;
+    $high = count($searches) - 1;
+
+    while ($low <= $high) {
+        $mid = floor(($low + $high) / 2);
+        if (strpos($searches[$mid], $searchValue) !== false) {
+            echo $pages[$mid];
             exit();
+        } elseif ($searchValue < $searches[$mid]) {
+            $high = $mid - 1;
         } else {
-            $response = "NOTFOUND";
+            $low = $mid + 1;
         }
-        $i++;
     }
+
+    $response = "NOTFOUND";
 }
+
 echo $response;
 ?>
